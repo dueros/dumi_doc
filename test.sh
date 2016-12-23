975221f2dd1ae042df8148992bbec99b112d9c54
wp@rmbp <wwwppp0801@qq.com>
2016-12-23 17:22:59 +0800

    add music

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 ajv -s dumi_schema.json -r news.schema.json -r weather.schema.json -d testdata/weather.json
 ajv -s dumi_schema.json -r news.schema.json -r weather.schema.json -d testdata/news.json
+ajv -s dumi_schema.json -r news.schema.json -r weather.schema.json -d testdata/music.json
